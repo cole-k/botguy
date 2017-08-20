4a392e9d31e718272b59feacf0243ed921ec1b88
Cole K <cole-k@users.noreply.github.com>
2017-08-19 17:10:30 -0700

    add help for everything but yam

--- help.js
+++ help.js
@@ -3,11 +3,11 @@
 var ArgParser = require('./argparse.js');
 
 var parser = new ArgParser(helpDefault);
-/*parser.addArg(['memeify','m'],helpMemeify);
+parser.addArg(['memeify','m'],helpMemeify);
 parser.addArg(['detonate','d'],helpDetonate);
 parser.addArg(['whois'],helpWhois);
 parser.addArg(['niceone','n1'],helpNiceOne);
-parser.addArg(['yam','y'],helpYam);*/
+//parser.addArg(['yam','y'],helpYam);
 
 function help(args, session) {
     parser.parse(args,session);
@@ -15,15 +15,64 @@ function help(args, session) {
 
 function helpDefault(args, session) {
     session.send(
-        ` Learn more about a particular command with _help command_
-        COMMANDNAME (alts) [flags] args - description
+        `Learn more about a particular command with _help command_\n
+        COMMANDNAME (alts) [flags] args - description\n
         memeify (m) [n] string- 'memeifies' a string\n
         detonate (d) [n] string - 'detonates' a string\n
-        whois string - ask about a person\n
+        whois person - ask about a person\n
         niceone (n1) [minus get list] person - give or list niceones\n
-        yam (y) [random featured get post] - retrieves or posts to the meme machine\n
+        yam (y) [random featured get post] - retrieves or posts to the meme machine
         `
     );
+}
+
+function helpMemeify(args, session) {
+    session.send(
+        `Memeify: Takes a string and increases the spaces between its characters successively\n
+        Called like: (memeify m) [n] string\n
+        Flags:\n
+              n - integer parameter (negative means reverse) for number of times (default 3)\n
+        Args:\n
+              string - string to memeify
+        `
+    )
+}
+
+function helpDetonate(args, session) {
+    session.send(
+        `Detonate: Takes a string and 'detonates' it\n
+        Called like: (detonate d) [n] string\n
+        Flags:\n
+              n - integer parameter for the type of detonation:\n
+              -2 takes transpose and reverses, -1 reverses, 2 takes transpose\n
+        Args:\n
+              string - string to detonate
+        `
+    )
+}
+
+function helpWhois(args, session) {
+    session.send(
+        `Whois: Takes a person and describes them\n
+        Called like: whois person\n
+        Supported people: botguy, here
+        `
+    )
+}
+
+function helpNiceOne(args, session) {
+    session.send(
+        `Niceone: Give or list nice ones\n
+        Called like: (niceone n1) [(minus m) get list] person\n
+        Flags:\n
+              [no flag] - add a nice one to person\n
+              minus (m) - subtract a niceone from person (works only if person has >= 1 nice ones)\n
+              get - get the nice ones of person (called without a person works the same as list)\n
+              list - lists all nice ones (takes no argument)\n
+        Args:\n
+              person - the target person
+        `
+    )
 }
 
 module.exports = help;
